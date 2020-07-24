0dfb65657f454fe16e0e29fe5f4728ebc423b438
youngjina <68758490+youngjina@users.noreply.github.com>
2020-07-24 18:33:16 -0400

    Update Extract_MutationRate_AllPossibleVariants_WellCoveredRegion.sh

--- Extract_MutationRate_AllPossibleVariants_WellCoveredRegion.sh
+++ Extract_MutationRate_AllPossibleVariants_WellCoveredRegion.sh
@@ -11,17 +11,17 @@ def doMutationType(cohort, mutationType):
 	p_2.wait()
 	print( "cmd_2" )
 
-	cmd_3 = "python /media/Dazs/youngji/Scripts/input_mr_eel.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_input_anno/ Absent_gnomAD_{mutationType}_Covered_90_percent_Positions_{cohort}_min-coverage_10_variantID.txt".format(mutationType = mutationType, cohort = cohort)
+	cmd_3 = "python input_mr_eel.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_input_anno/ Absent_gnomAD_{mutationType}_Covered_90_percent_Positions_{cohort}_min-coverage_10_variantID.txt".format(mutationType = mutationType, cohort = cohort)
 	p_3 = subprocess.Popen( cmd_3, shell=True)
 	p_3.wait()
 	print( "cmd_3" )
 
-	cmd_4 = "python /media/Dazs/youngji/Scripts/batch_mr_eel.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_input_anno/ Absent_gnomAD_{mutationType}_Covered_90_percent_Positions_{cohort}_min-coverage_10_variantID.txt /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_output_anno/".format(mutationType = mutationType, cohort = cohort)
+	cmd_4 = "python batch_mr_eel.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_input_anno/ Absent_gnomAD_{mutationType}_Covered_90_percent_Positions_{cohort}_min-coverage_10_variantID.txt /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_output_anno/".format(mutationType = mutationType, cohort = cohort)
 	p_4 = subprocess.Popen( cmd_4, shell=True)
 	p_4.wait()
 	print( "cmd_4" )
 
-	cmd_5 = "python /media/Dazs/youngji/Scripts/summary_mr_eel.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_output_anno/ Absent_gnomAD_{mutationType}_Covered_90_percent_Positions_{cohort}_min-coverage_10_sites_annotated.txt /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_anno_mut_rate/".format(mutationType = mutationType, cohort = cohort)
+	cmd_5 = "python summary_mr_eel.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_output_anno/ Absent_gnomAD_{mutationType}_Covered_90_percent_Positions_{cohort}_min-coverage_10_sites_annotated.txt /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/Absent_gnomAD_{mutationType}_anno_mut_rate/".format(mutationType = mutationType, cohort = cohort)
 	p_5 = subprocess.Popen( cmd_5, shell=True)
 	p_5.wait()
 	print( "cmd_5" )
@@ -36,12 +36,12 @@ def doMutationType(cohort, mutationType):
 	p_7.wait()
 	print( "cmd_7" )
 
-	cmd_8 = "python /media/Dazs/youngji/Scripts/mutation_rate_variantID.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/ Absent_gnomAD_{mutationType}_{cohort}_Carlson_mutability_90p_covered.txt Absent_gnomAD_{mutationType}_{cohort}_variantID_Carlson_mutability_90p_covered.txt".format(mutationType = mutationType, cohort = cohort)
+	cmd_8 = "python mutation_rate_variantID.py /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/ Absent_gnomAD_{mutationType}_{cohort}_Carlson_mutability_90p_covered.txt Absent_gnomAD_{mutationType}_{cohort}_variantID_Carlson_mutability_90p_covered.txt".format(mutationType = mutationType, cohort = cohort)
 	p_8 = subprocess.Popen( cmd_8, shell=True)
 	p_8.wait()
 	print( "cmd_8" )
 
-	cmd_9 = "Rscript /media/Dazs/youngji/Scripts/combine_positions_mutability.R /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/ Covered_90_percent_Positions_Gene_variantID_{cohort}_min-coverage_10_site.summary.txt Absent_gnomAD_{mutationType}_{cohort}_variantID_Carlson_mutability_90p_covered.txt Absent_gnomAD_{mutationType}_{cohort}_merged_Carlson_MutRate_Gene_variantID_min-coverage_10_site.summary.txt".format(mutationType = mutationType, cohort = cohort)
+	cmd_9 = "Rscript combine_positions_mutability.R /media/Jerry/youngji/Coverage_Analysis/Coverage_Summary_{cohort}_min-coverage_10/ Covered_90_percent_Positions_Gene_variantID_{cohort}_min-coverage_10_site.summary.txt Absent_gnomAD_{mutationType}_{cohort}_variantID_Carlson_mutability_90p_covered.txt Absent_gnomAD_{mutationType}_{cohort}_merged_Carlson_MutRate_Gene_variantID_min-coverage_10_site.summary.txt".format(mutationType = mutationType, cohort = cohort)
 	p_9 = subprocess.Popen( cmd_9, shell=True)
 	p_9.wait()
 	print( "cmd_9" )
